92b885dd021d4637a20fc9996ed6ff3852241421
Shashikant Sejwar <shashikant.sejwar@outlook.com>
2019-04-06 22:20:32 +0530

    Resolve visual studio warnings

--- EmployeeService/EmployeeService/Controllers/AccountController.cs
+++ EmployeeService/EmployeeService/Controllers/AccountController.cs
@@ -62,7 +62,7 @@ namespace EmployeeService.Controllers
             {
                 Email = User.Identity.GetUserName(),
                 HasRegistered = externalLogin == null,
-                LoginProvider = externalLogin != null ? externalLogin.LoginProvider : null
+                LoginProvider = externalLogin?.LoginProvider
             };
         }
 
@@ -308,7 +308,7 @@ namespace EmployeeService.Controllers
                         response_type = "token",
                         client_id = Startup.PublicClientId,
                         redirect_uri = new Uri(Request.RequestUri, returnUrl).AbsoluteUri,
-                        state = state
+                        state
                     }),
                     State = state
                 };
@@ -423,8 +423,10 @@ namespace EmployeeService.Controllers
 
             public IList<Claim> GetClaims()
             {
-                IList<Claim> claims = new List<Claim>();
-                claims.Add(new Claim(ClaimTypes.NameIdentifier, ProviderKey, null, LoginProvider));
+                IList<Claim> claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.NameIdentifier, ProviderKey, null, LoginProvider)
+                };
 
                 if (UserName != null)
                 {
